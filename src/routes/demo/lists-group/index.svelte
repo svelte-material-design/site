21e8a80caca807dcd037919d25941d86749b9d60
Ray Thurne Void <monkeythedev@gmail.com>
2020-12-07 02:33:43 +0100

    @smui/list/group configurator implementation

---
+++ src/routes/demo/lists-group/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Typography } from "@smui/core/typography";
+	import ListGroupConfigurator from "./_ListGroupConfigurator.svelte";
+
+	let currentTab: "list" | "listbox" | "navlist" = "list";
+</script>
+
+<svelte:options immutable={true} />
+
+<svelte:head>
+	<title>Switch - SMUI</title>
+</svelte:head>
+
+<section>
+	<Typography variant="headline2">Lists Group</Typography>
+
+	<ListGroupConfigurator />
+</section>
