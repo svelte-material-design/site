127ad0515327ea497546e8ba6854b6865a5391d2
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-06-14 08:42:27 +0200

    @svelte-material-design/core/button docs improvements

--- src/routes/demo/button/api/index.svelte
+++ src/routes/demo/button/api/index.svelte
@@ -1,7 +1,7 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-	import { Props, Events, Slots } from "../_api";
+	import { Props, Events, Slots } from "./_api";
 	import { ComponentTagHeading } from "src/components/components-api/headings";
 
 </script>
